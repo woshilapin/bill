01889a3fdf218fe9ce38df663f962d10a0cadf12
Jean SIMARD <woshilapin@tuziwo.info>
2015-06-04 10:49:27 +0200

    Display total prices

--- www/js/mainController.js
+++ www/js/mainController.js
@@ -16,21 +16,21 @@ angular.module('BillApp', [])
 		};
 		$scope.purchases = [
 			{
-				name:'resto Sola',
+				name:'Resto Sola',
 				payers: [
 					{people: 0, price: 0.0},
 					{people: 1, price: 58.8}
 					]
 			},
 			{
-				name:'tickets métro',
+				name:'Tickets métro',
 				payers: [
 					{people: 0, price: 12.6}
 					]
 			},
 			{
-				name:'bar du bout du monde',
-				payers:[
+				name:'Bar du bout du monde',
+				payers: [
 					{people: 0, price: 10.00},
 					{people: 1, price: 6.30},
 					{people: 4, price: 0.0}
@@ -44,7 +44,20 @@ angular.module('BillApp', [])
 		$scope.removePurchase = function(index) {
 			$scope.purchases.splice(index, 1);
 		};
-		$scope.addPayer = function(purchaseindex, newpeopleindex) {
-			$scope.purchases[purchaseindex].payers.push({people: newpeopleindex, price: 0.0});
+		$scope.pricePurchase = function(purchaseindex) {
+			var total = 0.0;
+			for(var payer of $scope.purchases[purchaseindex].payers) {
+				var price = parseFloat(payer.price);
+				if(!isNaN(price)) {
+					total += price;
+				}
+			}
+			return total;
+		}
+		$scope.addPayer = function(purchaseindex) {
+			$scope.purchases[purchaseindex].payers.push({people: $scope.purchases[purchaseindex].payers._newPayerIndex, price: 0.0});
+		}
+		$scope.removePayer = function(purchaseindex, payerindex) {
+			$scope.purchases[purchaseindex].payers.splice(payerindex, 1);
 		}
   }]);
